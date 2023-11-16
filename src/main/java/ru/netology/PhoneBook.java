59a086a077865c1194b8a882db4eda08f8392247
Tatarinov A <vekwork@bk.ru>
2023-11-16 22:33:09 +0300

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,7 +1,18 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    public Map<String, String> phoneBook = new HashMap<>();
+
     public int add(String name, String phone) {
-        return 0;
+        if (phoneBook.containsKey(name)) {
+            return phoneBook.size();
+        } else {
+            phoneBook.put(name, phone);
+        }
+
+        return phoneBook.size();
     }
 }
